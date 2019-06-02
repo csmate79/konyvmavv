0f5f26f7de278d8229e317381af92149e8061ccd
Csicsek Máté <csicsekmate79@gmail.com>
2019-06-02 18:47:53 +0200

    ..

---
+++ src/main/controllers/BelepesKezelo.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import main.Preferences;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ * FXML Controller class
+ *
+ * @author Csicsek Máté
+ */
+public class BelepesKezelo implements Initializable {
+
+    @FXML
+    private JFXTextField username;
+    @FXML
+    private JFXPasswordField jelszo;
+
+    private Preferences preference;
+
+    @FXML
+    private Label titleLabel;
+
+    /**
+     * Initializes the controller class.
+     * @param url - url
+     * @param rb - rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        preference = Preferences.getPreferences();
+    }
+
+    /**
+     * A belépés ablak.
+     * Ha megegyezik a config.txt-ben tárolt username meg a password
+     * és az itteni mezőkben beírtak, akkor továbbít a
+     * "/konyvtarkezelo/ui/main/main.fxml"-hez.
+     *
+     * Ha hibás akkor errort dob fel.
+     *
+     * @param event - gombhoz
+     */
+    @FXML
+    private void loginButtAct(ActionEvent event) {
+        titleLabel.setText("Könyvtárkezelő belépés");
+        titleLabel.setStyle("-fx-background-color:lightgrey");
+
+        String uname = username.getText();
+        String pword = jelszo.getText();
+
+        if(uname.equals(preference.getUsername()) && pword.equals(preference.getPassword())){
+            ((Stage)username.getScene().getWindow()).close();
+            loadMain();
+            Logger.getLogger(BelepesKezelo.class.getName()).log(Level.INFO, "Sikerült belépni");
+        } else if (username != null && jelszo != null) {
+            titleLabel.setText("Töltsd ki a mezőket");
+            titleLabel.setStyle("-fx-background-color:#d32f2f"); //Ha sikertelen a belépés, akkor megváltozik a titleLabel színe pirosra.
+        }  else {
+            titleLabel.setText("Hibás próbálkozás");
+            titleLabel.setStyle("-fx-background-color:#d32f2f"); //Ha sikertelen a belépés, akkor megváltozik a titleLabel színe pirosra.
+            Logger.getLogger(BelepesKezelo.class.getName()).log(Level.INFO, "Sikerült a belépés");
+        }
+    }
+
+    /**
+     * Bezárjuk a "Belépés" ablakot.
+     *
+     * @param event - gombhoz
+     */
+    @FXML
+    private void cancelButtAct(ActionEvent event) {
+        System.exit(0);
+    }
+
+    /**
+     * A main ablakra való lépés függvényben.
+     */
+    private void loadMain() {
+        try {
+            Parent parent = FXMLLoader.load(getClass().getResource("/main.fxml"));
+            Stage stage = new Stage(StageStyle.DECORATED);
+            stage.setTitle("Könyvtárkezelő");
+            stage.setScene(new Scene(parent));
+            stage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(MainKezelo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
